9fb728b3478a90f18c49a83c0a3077bed7fbe3e2
Jose Hernandez <r2h@adminisatorsmbp.home>
2020-07-14 12:59:18 -0400

    fixed footer icon anschors by adding null

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,8 +4,8 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer id="main-footer">
-            <a href="https://www.linkedin.com/in/jose-hernandez-43b87b1aa/" className="fa fa-linkedin-square"></a>
-            <a href="https://github.com/jjosee9" className="fa fa-github"></a>
+            <a href="https://www.linkedin.com/in/jose-hernandez-43b87b1aa/" className="fa fa-linkedin-square">{null}</a>
+            <a href="https://github.com/jjosee9" className="fa fa-github">{null}</a>
             <p> Jose H &amp; copy; 2020, All Rights Reserved</p>
         </footer>
     )
