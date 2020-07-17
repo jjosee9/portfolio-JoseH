07e6b338f92972e144880fb2ebeb3f64e3044f3b
Jose Hernandez <r2h@adminisatorsmbp.home>
2020-07-17 14:18:32 -0400

    updated design

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
                 <section>
                     <div class="hero-container">
                         <div className="transbox">
-                            <h2>Welcome to my portfolio</h2>
+                            <h2 className="welcome">WELCOME TO MY PORTFOLIO</h2>
                             <h2>JOSE HERNANDEZ</h2>
                         </div>
                     </div>
@@ -19,7 +19,7 @@ const Home = () => {
                 <section>
                     <div class="about">
                         <img src={joseh} alt="profile" />
-                        <p>I am a passionate recent gradute of the intense 6 month Road to Hire program.
+                        <p>I am a passionate recent graduate of the intense 6 month Road to Hire program.
                            Through Road to Hire I have aquired both technical and professional skills. During my journey 
                            in Road to Hire I was guided by awesome life and tech mentors.
                         <br></br>
@@ -28,8 +28,6 @@ const Home = () => {
                             <br></br>
                             Back end Skills: Node.Js, MySql, and MongoDb 
                         </p>
-                        <p class="line-break margin-top-10"></p>
-                        {/* <p class="margin-top-10">The UGG brand began to symbolize those who embraced sport and a relaxed, active lifestyle. More than that, an emotional connection and a true feeling of love began to grow for UGG boots, just as Brian had envisioned. People didn't just like wearing UGG boots, they fell in love with them and literally could not take them off. By the end of the 90's, celebrities and those in the fashion world took notice of the UGG brand. A cultural shift occurred as well - people were embracing, and feeling empowered, by living a more casual lifestyle and UGG became one of the symbols of this lifestyle. By 2000, a love that began on the beaches had become an icon of casual style. It was at this time that the love for UGG grew in the east, over the Rockies and in Chicago. In 2000, UGG Sheepskin boots were first featured on Oprah's Favorite Things® and Oprah emphatically declared that she "LOOOOOVES her UGG boots." From that point on, the world began to notice.</p> */}
                     </div>
                 </section>
             </div>
