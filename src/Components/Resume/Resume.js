3329f0593f96c355f007eba6bf25174b558a4aeb
Jose Hernandez <r2h@adminisatorsmbp.home>
2020-07-17 16:03:48 -0400

    down button added

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -5,8 +5,15 @@ import resumeJH from '../../Images/resume-j.png'
 const Resume = () => {
     return (
         <>
-         <img className="resume-img" src={resumeJH} alt="Resume" />
-        </>  
+            <img className="resume-img" src={resumeJH} alt="Resume" />
+            <a href={require("../../Images/resume-j.png")} download="myFile">
+                <button className="download-btn"><i class="fa fa-download"></i>Download</button>
+
+                </a>
+            {/* <a className href="../../Images/resume-j.png" download>
+               <button className="resume__button"><i class="fa fa-download"></i> Download</button>
+            </a> */}
+        </>
     )
-}                       
+}
 export default Resume
